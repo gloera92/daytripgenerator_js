9331c6372db47559761210604ce2c3cbdd7a7529
George Loera <gloera8325@gmail.com>
2021-03-06 22:35:07 -0600

    Added switch/case that will eventually allow user to re-randomize specific choice in trip.

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -1,9 +1,9 @@
 "use strict"
 
-let destination = ['California', 'Arkansas', 'Las Vegas'];
-let restaurant = ['Italian', 'Steak House', 'Korean BBQ'];
-let transportation = ['Train', 'Car', 'Air Plane'];
-let entertainment = ['Concert', 'Movie', 'Amusement Park'];
+let destination = ['California', 'Arkansas', 'Colorado', 'Las Vegas'];
+let restaurant = ['Italian', 'Steak House', 'Korean BBQ', 'Pizza'];
+let transportation = ['Train', 'Car', 'Air Plane', 'Helicopter'];
+let entertainment = ['Concert', 'Movie', 'Amusement Park', 'Beach'];
 
 function randomTripGen (array) {
 
@@ -11,22 +11,86 @@ function randomTripGen (array) {
 
     return value;
 }
-    
+ 
 let randomDestination = randomTripGen(destination);         //prints random destination
 
 let randomRestaurant = randomTripGen(restaurant);           //prints random restaurant
-console.log(randomRestaurant);
 
 let randomTravelType = randomTripGen(transportation);       //prints random transportation
-console.log(randomTravelType);
 
-let randomEntertainment = randomTripGen(entertainment);         //prints random entertainment
-console.log(randomEntertainment);
+let randomEntertainment = randomTripGen(entertainment);     //prints random entertainment 
+
+let destinationResult = prompt(`Here is your randomized trip!  ${randomDestination} is your destination, ${randomRestaurant} is a good spot for food, ${randomTravelType} is how you will travel, and ${randomEntertainment} is your entertainment!  Will you be taking this trip?`);  // alerts user his randomized trip
+
+//let destinationChoice = prompt(`Was ${destinationResult} a good selection?`);           //allows the user to pick if the destination is suitable.
+
+// function otherSelection (selection) {
+//     let j = prompt(`Is ${selection[i]} a good choice?`)
+//    for(let i = 0; i < selection.length; i++) 
+   
+//     if(j != "yes") 
+//         j
+    
+//     else { 
+//         return;
+//     }
 
 
-let destinationResult = alert(`This will be your randomized trip!  ${randomDestination} is your destination, ${randomRestaurant} is a good spot for food, ${randomTravelType} is how you will travel, and ${randomEntertainment} is your entertainment!`);          //alerts user his randomized trip
+if(destinationResult === "yes"|| destinationResult === "Yes") {      //if user chooses yes, it will enter console log.  if user chooses no it will ask them which part of trip they want to change.
+    console.log("Have a great trip!")
+}
+else {
+    let chooseAnotherType = prompt(`Which part of the trip would you like to change?  (1) Destination,         (2) Restaurant, (3) Transportation, or (4) Entertainment?`);
+    chooseAnotherType
 
-//let destinationChoice = prompt(`Was ${randomDestination} a good selection?`);           //allows the user to pick is the destination is suitable
+switch(chooseAnotherType) {       //if user choose 1-4, it will select which part of trip they wanna change an re-roll that part of the trip.
+    case "1":
+
+    otherSelection(destination)
+
+    break;
+
+    case "2":
+
+    otherSelection(restaurant)
+
+    break;
+
+    case "3":
+
+    otherSelection(transportation)
+
+    break;
+
+    case "4": 
+
+    otherSelection(entertainment)
+
+    break;
+
+    default:
+        console.log("try again")
+
+        }
+    }
 
 
 
+// if(destinationResult === "1" ) {
+//     chooseArray(desination);
+
+
+// }
+// else if(destinationResult === "2") {
+//     chooseArray(restaurant)
+// }
+// else if(destinationResult === 3) {
+//     chooseArray(transportation)
+// }
+// else if(destinationResult === entertainment) {
+//     chooseArray(entertainment)
+// }
+// else {
+//     console.log("have a good trip")
+// }
+// }
